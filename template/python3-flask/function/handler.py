1aeea26bfa0eb55f7221f22380a59d22e4294031
Oleg Dolya <oleg.dolya@gmail.com>
2020-08-10 14:25:50 +0300

    merge from lihopentium

--- template/python3-flask/function/handler.py
+++ template/python3-flask/function/handler.py
@@ -1,7 +1,11 @@
+import time
+
+
 def handle(req):
     """handle a request to the function
     Args:
         req (str): request body
     """
-
+    time.sleep(0.01)
+    print("hello world")
     return req
